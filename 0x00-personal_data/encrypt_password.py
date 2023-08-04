8a09bedc66674164c9f24975beb39f50e593ec20
Chidi <ajaerohc@gmail.com>
2023-08-04 03:40:41 +0100

    SQL

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for encrypting passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Module Hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Module Checks is a hashed password was formed 
+    from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
